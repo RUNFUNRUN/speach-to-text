b83cda766d0478698bd866e5acfd87d02056e572
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-12-11 15:50:17 +0900

    add spinner

--- src/app/_components/ui/FixButton.tsx
+++ src/app/_components/ui/FixButton.tsx
@@ -3,25 +3,38 @@ import { Button } from '@/components/ui/button';
 import { fixText } from '@/lib/openai';
 import { Alert } from './Alert';
 import { useState } from 'react';
+import { Loader2 } from 'lucide-react';
 
 export const FixButton = ({ apiKey, text, setText }: FixButtonProps) => {
+  const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const alert: AlertInfo = { title: 'something wrong', description: 'fix failed' };
 
   const handleFix = async () => {
     if (text === '') return;
+    setLoading(true);
     const newText = await fixText(apiKey, text);
     if (!newText) {
       setOpen(true);
+      setLoading(false);
+      return;
     }
     setText(newText);
+    setLoading(false);
   };
 
   return (
     <>
-      <Button size="sm" variant="secondary" onClick={handleFix}>
-        Fix typos with AI
-      </Button>
+      {!loading ? (
+        <Button size="sm" className="w-36" variant="secondary" onClick={handleFix}>
+          Fix typos with AI
+        </Button>
+      ) : (
+        <Button size="sm" className="w-36" variant="secondary" onClick={handleFix}>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          Fixing
+        </Button>
+      )}
       <Alert info={alert} open={open} setOpen={setOpen} />
     </>
   );
